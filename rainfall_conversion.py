1ebe8a275ecedb972f7dc3db4f5553da05e151fc
Luca Possenti <L.Possenti@uea.ac.uk>
2019-03-14 15:16:50 +0000

    second version rainfall

--- rainfall_conversion.py
+++ rainfall_conversion.py
@@ -1,10 +1,12 @@
 """A library to perform rainfall unit conversions"""
 
 def inches_t_mm(inches):
-"""Convert inches to millimeters
+"""Convert inches to millimeters GDASGD
 
 Arguments:
 inches -- the rainfall in inches"""
 
 mm = inches * 25.4
 return mm
+
+
